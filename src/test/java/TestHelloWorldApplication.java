196c60f1391d2c7773dcae1f6056693badc6a13c
AustinGrey <corey.crapo@gmail.com>
2020-01-20 18:14:35 -0700

    Adding tests for main app code

---
+++ src/test/java/TestHelloWorldApplication.java
@@ -0,0 +1,17 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+import com.example.helloworld.HelloWorldApplication;
+
+public class TestHelloWorldApplication {
+
+    @Test
+    public void testGetName(){
+        // Setup
+        HelloWorldApplication app = new HelloWorldApplication();
+
+        // Run
+        assertEquals(app.getName(), "hello-world")
+    }
+}
